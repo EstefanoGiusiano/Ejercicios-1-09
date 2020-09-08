1927562e790e65423dc0d1e14d6e9001290d287e
EstefanoGiusiano <estefanogiusiano2607@gmail.com>
2020-09-08 19:48:16 -0300

    Tablero

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,12 +1,15 @@
 ﻿using System;
-
+using Tablero;
+using TextFileReader;
 namespace PII_Game_Of_Life
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            TextFileReader reader= new TextFileReader("../../assets/board.txt");
+            Tablero board=new Tablero(reader.ReadTextFile());
+            Console.WriteLine(board);
         }
     }
 }
